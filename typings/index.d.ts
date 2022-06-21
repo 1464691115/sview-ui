5f62a7686f9e41d284bfc40507350f05a1de1df7
1464691115 <1464691115@qq.com>
2022-06-21 19:33:00 +0800

    bug：兼容 volar

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,3 +4,11 @@ declare namespace UniApp {
     interface Uni {
     }
 }
+
+
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
