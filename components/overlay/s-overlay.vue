3fd34630feb5b7c138f59081365d70c1b63c4a77
1464691115 <1464691115@qq.com>
2022-06-27 17:48:11 +0800

    🐛bug：遮罩层无法点击问题

--- components/overlay/s-overlay.vue
+++ components/overlay/s-overlay.vue
@@ -1,36 +1,37 @@
 <template>
-  <transition name="fade" :duration="props.duration || 300">
+  <Transition>
     <view
-      v-if="props.show"
+      v-if="props.modelValue"
       class="s-overlay"
       :style="overlayStyle"
       @click="handleTapOverlay"
     >
       <slot />
     </view>
-  </transition>
+  </Transition>
 </template>
 <script lang="ts" setup>
 import { computed, CSSProperties } from "vue";
 interface Props {
-  show?: boolean;
+  modelValue?: boolean;
   zIndex?: number;
   duration?: number;
   opacity?: number;
 }
 const props = defineProps<Props>();
 const emits = defineEmits<{
-  (e: "update:show", val: boolean);
+  (e: "update:modelValue", val: boolean);
   (e: "click", event: Event);
 }>();
 
 const overlayStyle = computed<CSSProperties>(() => ({
   zIndex: props.zIndex || 10070,
   backgroundColor: `rgba(0, 0, 0, ${props.opacity || 0.5})`,
+  transitionDuration: (props.duration || 300) + "ms",
 }));
 
 function handleTapOverlay(e) {
-  emits("update:show", !props.show);
+  emits("update:modelValue", !props.modelValue);
   emits("click", e);
 }
 </script>
@@ -41,5 +42,13 @@ function handleTapOverlay(e) {
   left: 0;
   width: 100vw;
   height: 100vh;
+}
+</style>
+
+<style>
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
+
+import { ref } from "vue";
+
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+const show = ref(true);
 </script>
 
 <template>
-  <s-overlay :show="false"/>
+  <s-overlay v-model="show" />
   <!-- <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
 </template>
